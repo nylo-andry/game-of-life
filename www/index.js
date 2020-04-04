cf83e09709806ab88f21f9ae67a1b211e99d1ac2
Ny Fanilo Andrianjafy <ny.fanilo@gmail.com>
2020-04-03 21:37:58 -0400

    Change cell size

--- www/index.js
+++ www/index.js
@@ -1,7 +1,7 @@
 import { Universe, Cell } from "game-of-life";
 import { memory } from "game-of-life/game_of_life_bg";
 
-const CELL_SIZE = 5; // px
+const CELL_SIZE = 7; // px
 const GRID_COLOR = "#CCCCCC";
 const DEAD_COLOR = "#FFFFFF";
 const ALIVE_COLOR = "#000000";
